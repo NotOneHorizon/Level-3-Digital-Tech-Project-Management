e42a8690979ebd973f20cb63bf65a40eab313627
NotOneHorizon <80720950+NotOneHorizon@users.noreply.github.com>
2021-05-12 21:33:48 +1200

    Add files via upload

---
+++ StartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartButtomGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
