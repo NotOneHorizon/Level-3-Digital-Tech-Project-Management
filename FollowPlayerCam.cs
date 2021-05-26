3a6af45ffce6da90b944fd7c3606f759354bc96f
NotOneHorizon <80720950+NotOneHorizon@users.noreply.github.com>
2021-05-26 22:51:10 +1200

    Add files via upload

---
+++ FollowPlayerCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCam : MonoBehaviour
+{
+    public float DampTime = 10f;
+    public Transform Player;
+    public float XOffset = 0;
+    public float YOffset = 0;
+
+	private float margin = 0.1f;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Player)
+		{
+			float targetX = Player.position.x + XOffset;
+			float targetY = Player.position.y + YOffset;
+
+			if (Mathf.Abs(transform.position.x - targetX) > margin)
+				targetX = Mathf.Lerp(transform.position.x, targetX, 1 / DampTime * Time.deltaTime);
+
+			if (Mathf.Abs(transform.position.y - targetY) > margin)
+				targetY = Mathf.Lerp(transform.position.y, targetY, DampTime * Time.deltaTime);
+
+			transform.position = new Vector3(targetX, targetY, transform.position.z);
+		}
+	}
+}
---
+++ GameConductor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameConductor : MonoBehaviour
+{
+    bool gameHasConcluded = false;
+
+    public float restartDelay = 1f;
+
+    public void EndGame()
+    {
+        if (gameHasConcluded == false)
+        {
+            gameHasConcluded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
